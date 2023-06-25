c3407a2fbbd61217a8ecfca70d2c3e785e1c2721
Yahya Alataş <yahya.alatas.mdbf17@iste.edu.tr>
2023-06-25 20:14:44 +0300

    fix

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UpInsertModule } from './up-insert/up-insert.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+       envFilePath:'.env',
+       isGlobal:true,
+       cache:false,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UpInsertModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
